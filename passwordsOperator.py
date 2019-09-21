39c297a0225800ee523255f85bb75b57c2bd9e80
Candy <metal_screm_sp@hotmail.com>
2019-09-21 02:10:57 -0500

    Se agrega la funcionalidad para buscar contraseñas

--- passwordsOperator.py
+++ passwordsOperator.py
@@ -17,7 +17,7 @@ class PasswordsOperator:
 
         #We validate if the file where the passwords are stored exists
         #If not exists then we create a new file with the first password
-        if not os.path.exists(self.containerDirectory + self.fileName):
+        elif not os.path.exists(self.containerDirectory + self.fileName):
             passwordsFile = open(self.containerDirectory + self.fileName, "w")
             passwordsFile.write("Site name: " + site + " - Email/UserName: " + emailOrUser + " - Password: " + crypt.crypt(password, "encrypt") + "\r\n")
             passwordsFile.close()
@@ -26,9 +26,71 @@ class PasswordsOperator:
         else:
             #We append the new password to the password's file
             passwrodsFile = open(self.containerDirectory + self.fileName, "a+")
-            passwrodsFile.write("Site name: " + site + " - Email/UserName: " + emailOrUser + " - Password: " + crypt.crypt(password, "encrypt") + "\r\n")
+            passwrodsFile.write("Site Name: " + site + " - Email/UserName: " + emailOrUser + " - Password: " + crypt.crypt(password, "encrypt") + "\r\n")
         
 
         succesAlert = QMessageBox()
         succesAlert.setText("Password Stored Successfully")
         succesAlert.exec_()
+
+    def searchPassword(self, searchByValue, valueToSearch):
+        #If the directory or file where the passwords are stored doesn't exist we return "404"
+        if (self.validatePasswordsFilesExistance()):
+            passwordsFile = open(self.containerDirectory + self.fileName, "r")
+
+            if(passwordsFile.mode == "r"):
+                passwordsInfoArray = []
+
+                fileContent = passwordsFile.read()
+
+                #We first clean the content from white spaces
+                fileContent = fileContent.replace(" ", "")
+                #We split every password information, each one of them are stores separated by new lines
+                fileContent = fileContent.split("\n")
+
+                #We use "-1" in our for cicle to avoid adding and empty new line to the array
+                for passwordInfo in range(len(fileContent) - 1):
+                    passwordsInfoArray.append(fileContent[passwordInfo].split("-"))
+                
+                #We define the index that we are going to use to search in the passwordsInfoArray
+                #The index is defined by the value selected by the user on the searchPasswordScreen's combobox
+                if(searchByValue == "Site Name"):
+                    searchByIndex = 0
+                else:
+                    searchByIndex = 1
+                
+                #Variable where the searched password is going to be stored
+                passwordInfoFound = ""
+
+                #We iterate throw every element in the passwordsInfoArray
+                for passwordInfo in passwordsInfoArray:
+                    
+                    #We split the value "searchByIndex", that we setted above, in the "passwordInfo" array
+                    arrayValue = passwordInfo[searchByIndex].split(":")
+                    #We take the second value from the element in the array, which corresponds to the "Site Name" or "Emial/UserName" value
+                    arrayValue = arrayValue[1]
+
+                    #If we found any value that match, we join every element in the array where is that value, using "-" to separate each value
+                    if(arrayValue == valueToSearch):
+                        passwordInfoFound = "-".join(passwordInfo)
+
+                #If the variable, where we were going to store the password once we found it, is setted to it's default vaule
+                #That means that we didn't find any password that match, so we return "405"
+                if(passwordInfoFound == ""):
+                    return "405"
+                #if we find the password we just return it as a string
+                else:
+                    return passwordInfoFound
+        
+        else:
+            return "404"
+                
+        
+    def validatePasswordsFilesExistance(self):
+        #If the directory or file where the passwords are stored doesn't exist we return "404"
+        if not os.path.exists(self.containerDirectory):
+            return False
+        elif not os.path.exists(self.containerDirectory + self.fileName):
+            return False
+        else:
+            return True
--- storeNewPassword.py
+++ storeNewPassword.py
@@ -18,56 +18,66 @@ class StoreNewPassword(QMainWindow):
 
     #Draws the screen to store a new password
     def drawWindow(self):
-        #Draws label and text box for site's name
-        sitenNamelabel = QLabel(self)
-        sitenNamelabel.setText("Site Name")
-        sitenNamelabel.move(20, 0)
+        #Draws the button to go back
+        backbtn = QPushButton("Go back", self)
+        backbtn.clicked.connect(self.goBack)
+        backbtn.resize(80, 25)
+        backbtn.move(120, 5)
+
+        #Draws label and textbox for site's name
+        sitenNameLabel = QLabel(self)
+        sitenNameLabel.setText("Site Name")
+        sitenNameLabel.setFixedSize(165, 25)
+        sitenNameLabel.move(20, 25)
         self.siteNameTxtBox = QLineEdit(self)
         self.siteNameTxtBox.resize(165, 25)
-        self.siteNameTxtBox.move(20, 25)
+        self.siteNameTxtBox.move(20, 50)
 
         #Draws label and textbox for email or username
         emailOrUserLabel = QLabel(self)
         emailOrUserLabel.setText("Email or UserName")
-        emailOrUserLabel.move(20, 60)
-        self.emailOrUserTextbox = QLineEdit(self)
-        self.emailOrUserTextbox.resize(165, 25)
-        self.emailOrUserTextbox.move(20, 85)
+        emailOrUserLabel.setFixedSize(165, 25)
+        emailOrUserLabel.move(20, 85)
+        self.emailOrUserTextBox = QLineEdit(self)
+        self.emailOrUserTextBox.resize(165, 25)
+        self.emailOrUserTextBox.move(20, 110)
 
         #Draws label and textbox for password
         passwordLabel = QLabel(self)
         passwordLabel.setText("Password")
-        passwordLabel.move(20, 120)
+        passwordLabel.setFixedSize(165, 25)
+        passwordLabel.move(20, 145)
         self.passwordTextBox = QLineEdit(self)
         self.passwordTextBox.setEchoMode(QLineEdit.Password)
         self.passwordTextBox.resize(165, 25)
-        self.passwordTextBox.move(20, 145)
+        self.passwordTextBox.move(20, 170)
 
         #Draws label and textbox for confirm password
         confirmPasswordLabel = QLabel(self)
         confirmPasswordLabel.setText("Confirm Password")
-        confirmPasswordLabel.move(20, 180)
+        confirmPasswordLabel.setFixedSize(165, 25)
+        confirmPasswordLabel.move(20, 205)
         self.confirmPasswordTextBox = QLineEdit(self)
         self.confirmPasswordTextBox.setEchoMode(QLineEdit.Password)
         self.confirmPasswordTextBox.resize(165, 25)
-        self.confirmPasswordTextBox.move(20, 205)
+        self.confirmPasswordTextBox.move(20, 230)
 
         #Draws the button to store the password and associates a event to it
         storePasswordbtn = QPushButton("Store Password", self)
         storePasswordbtn.clicked.connect(self.storePassword)
         storePasswordbtn.resize(165, 25)
-        storePasswordbtn.move(20, 245)
+        storePasswordbtn.move(20, 265)
 
         #Draws the window
         self.setGeometry(200, 200, self.windowWidth, self.windowHeigh)
-        self.setWindowTitle('PasswordsVault - Store New Password')    
+        self.setWindowTitle('Store New Password')    
         self.show()
     
     #Check if all the fields were filled if not returns false
     #@return false/true: Return a boolean value, True if all the fields were filled or False otherwise
     def checkFieldsValues(self):
         validInput = False
-        if((self.siteNameTxtBox.text() == "") or (self.emailOrUserTextbox.text() == "") or (self.passwordTextBox.text() == "") or (self.confirmPasswordTextBox.text() == "")):
+        if((self.siteNameTxtBox.text() == "") or (self.emailOrUserTextBox.text() == "") or (self.passwordTextBox.text() == "") or (self.confirmPasswordTextBox.text() == "")):
             error = QMessageBox()
             error.setText("Fill all fields")
             error.exec_()
@@ -85,8 +95,14 @@ class StoreNewPassword(QMainWindow):
     def storePassword(self):
         if(self.checkFieldsValues()):
             passwordsOpert = passwordsOperator.PasswordsOperator()
-            passwordsOpert.storePassword(self.siteNameTxtBox.text(), self.emailOrUserTextbox.text(), self.passwordTextBox.text())
+            passwordsOpert.storePassword(self.siteNameTxtBox.text(), self.emailOrUserTextBox.text(), self.passwordTextBox.text())
 
             self.mainScr = mainScreen.MainScreen()
             self.close()
+
+    #Function that allows the user to go back to the main screen
+    def goBack(self):
+        self.mainScr = mainScreen.MainScreen()
+        self.close()
+
         
